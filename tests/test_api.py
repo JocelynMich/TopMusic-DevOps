7f6b1ef1c2a2fdf582e4cb3e0261083862c06e1c
JocelynMich <98039944+JocelynMich@users.noreply.github.com>
2025-04-06 21:30:02 -0600

    Edit en tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,10 @@
 import pytest
 from fastapi.testclient import TestClient
 from backend.main import app
+from jose import jwt
+from datetime import datetime, timedelta
+SECRET_KEY = "2530"
+ALGORITHM = "HS256"
 
 client = TestClient(app)
 
@@ -16,9 +20,21 @@ def test_songs():
     response = client.get("/songs/")
     assert response.status_code == 200
 
+def create_test_token():
+    payload = {
+        "sub": "admin_user",
+        "role": "admin",
+        "exp": datetime.utcnow() + timedelta(minutes=60)
+    }
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
 def test_create_playlist():
-    response = client.get("/create_playlist/")
-    assert response.status_code == 200
+    token = create_test_token()
+    response = client.get(
+        "/create_playlist/",
+        headers={"token": token}
+    )
+    assert response.status_code == 200 or response.status_code == 500
 
 
 
